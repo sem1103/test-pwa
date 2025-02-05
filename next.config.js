58a472b12ae8fb51e7134d48d109cba3c21a4710
sem1103 <sem.sem.geo.az1998@mail.ru>
2025-02-05 14:16:54 +0400

    s

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development', // отключает PWA в dev-режиме
-  register: true,
-  skipWaiting: true,
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
 });
 
 module.exports = withPWA({
-  reactStrictMode: true, // Включает строгий режим React
+  // Your Next.js config
 });
